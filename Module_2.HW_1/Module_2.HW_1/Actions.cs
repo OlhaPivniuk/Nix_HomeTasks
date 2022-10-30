5cae0206146e30be68f00d619989c3bf9188f72e
OlhaPivniuk <pivnyuk_olga@ukr.net>
2022-10-30 15:49:23 +0200

    Add task 2

---
+++ Module_2.HW_1/Module_2.HW_1/Actions.cs
@@ -0,0 +1,30 @@
+﻿namespace Module2HW1
+{
+    public class Actions
+    {
+        readonly Logger logger = Logger.GetInstance();
+
+        public Result StartMethod()
+        {
+            logger.WriteLog($"Start method: {StartMethod}", LogType.Info);
+            Result result = new();
+            result.Status = true;
+            return result;
+        }
+        public Result SkippedLogicInMethod()
+        {
+            logger.WriteLog($"Skipped logic in method: {StartMethod}", LogType.Warning);
+            Result result = new();
+            result.Status = true;
+            return result;
+        }
+        public Result ErrorMethod()
+        {
+            logger.WriteLog($"Method: {ErrorMethod}", LogType.Error);
+            Result result = new();
+            result.Status = false;
+            result.ErrorMessage = "I broke a logic";
+            return result;
+        }
+    }
+}
