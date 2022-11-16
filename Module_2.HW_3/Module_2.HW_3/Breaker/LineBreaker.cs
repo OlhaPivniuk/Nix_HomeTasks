35597323077104ae78705c2d2158a0702128b0f5
OlhaPivniuk <pivnyuk_olga@ukr.net>
2022-11-16 19:26:29 +0200

    Add Module_2_HW_3

---
+++ Module_2.HW_3/Module_2.HW_3/Breaker/LineBreaker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2.HW_3.Breaker
+{
+    public class LineBreaker
+    {
+        static public string[] ReturnWordArr(char symbol, string str, short index)
+        {
+            return str.Split(new char[] { symbol }, StringSplitOptions.RemoveEmptyEntries).Skip(index).ToArray();
+        }
+    }
+}
---
+++ Module_2.HW_3/Module_2.HW_3/Interface/IWriteToFiles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module_2.HW_3.Interface
+{
+    public interface IWriteToFiles
+    {
+        void WriteToFiles(string fileName);
+    }
+}
