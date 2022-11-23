b3feb19a02c0770e308d635a2810756b84cdaf45
OlhaPivniuk <pivnyuk_olga@ukr.net>
2022-11-23 16:42:15 +0200

    Added HW 2 of Module 3

---
+++ Module_3.HW_2/Module_3.HW_2/Interfaces/IContact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_3.HW_2.Interfaces
+{
+    public interface IContact
+    {
+        string FirstName { get; init; }
+        string FullName { get; }
+        string LastName { get; init; }
+    }
+}
---
+++ Module_3.HW_2/Module_3.HW_2/Models/Contact.cs
@@ -0,0 +1,38 @@
+﻿using Module_3.HW_2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_3.HW_2.Models
+{
+    public class Contact : IContact
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string Collection { get; set; }
+        public string FullName
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(FirstName) && !string.IsNullOrEmpty(LastName))
+                {
+                    return $"{FirstName} {LastName}";
+                }
+                else if (!string.IsNullOrEmpty(FirstName) && string.IsNullOrEmpty(LastName))
+                {
+                    return $"{FirstName}";
+                }
+                else if (string.IsNullOrEmpty(FirstName) && !string.IsNullOrEmpty(LastName))
+                {
+                    return $"{LastName}";
+                }
+                else
+                {
+                    throw new ArgumentNullException("First Name and Last Name are null or empty!");
+                }
+            }
+        }
+    }
+}
