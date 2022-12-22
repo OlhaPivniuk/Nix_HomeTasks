493a106421402d753cfa2fa786cfcba3a54e019a
OlhaPivniuk <pivnyuk_olga@ukr.net>
2022-12-22 22:49:59 +0200

    Added Module_3.HW_4

---
+++ Module_3.HW_4/Module_3.HW_4/Module_3.HW_4/TaskLinq/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_3.HW_4.TaskLinq
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public double Phone { get; set; }
+    }
+}
