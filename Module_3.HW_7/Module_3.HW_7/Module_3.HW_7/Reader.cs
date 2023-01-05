3f16e106823dec07f50adf6deb6eabc8d0ec3e6f
OlhaPivniuk <pivnyuk_olga@ukr.net>
2023-01-05 17:55:42 +0200

    Added HW 7 of Module 3

---
+++ Module_3.HW_7/Module_3.HW_7/Module_3.HW_7/Reader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_3.HW_7
+{
+    internal class Reader
+    {
+        public async Task<int> TextReader(string path)
+        {
+            string text = await File.ReadAllTextAsync(path);
+
+            Int32.TryParse(text, out int result);
+            return result;
+        }
+    }
+}
