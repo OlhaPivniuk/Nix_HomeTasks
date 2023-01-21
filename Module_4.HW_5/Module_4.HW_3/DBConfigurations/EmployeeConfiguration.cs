da429e2632a5a3a0a77d1fc900af7175b9dc58df
OlhaPivniuk <pivnyuk_olga@ukr.net>
2023-01-21 21:13:19 +0200

    Added Module 5 of HW 5

---
+++ Module_4.HW_5/Module_4.HW_3/DBConfigurations/EmployeeConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Module_4.HW_5.Entities;
+
+namespace Module_4.HW_5.DBConfigurations
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.ToTable("Employee").HasKey(e => e.EmployeeId);
+            builder.Property(e => e.FirstName).HasMaxLength(50).IsRequired();
+            builder.Property(e => e.LastName).HasMaxLength(50).IsRequired();
+            builder.Property(e => e.HiredDate).IsRequired().HasColumnType("date");
+            builder.Property(e => e.OfficeId).IsRequired();
+            builder.Property(e => e.TitleId).IsRequired();
+            builder.HasOne(d => d.Title)
+                .WithMany(p => p.Employees)
+                .HasForeignKey(d => d.TitleId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.HasOne(e => e.Office)
+                .WithMany(o => o.Employees)
+                .HasForeignKey(f => f.OfficeId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Module_4.HW_5/Module_4.HW_3/Entities/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_4.HW_5.Entities
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public List<Project> Projects { get; set; } = new List<Project>();
+    }
+}
