c57908de928e29daaf26298e287b7f47cfd71b12
OlhaPivniuk <pivnyuk_olga@ukr.net>
2023-01-21 21:23:09 +0200

    Changed name config.json file

--- Module_4.HW_5/Module_4.HW_3/Starter.cs
+++ Module_4.HW_5/Module_4.HW_3/Starter.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace Module_4.HW_5
 {
@@ -12,9 +13,9 @@ namespace Module_4.HW_5
     {
         public void Run()
         {
-            IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("config.json").Build();
             var dbOptionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
-            var connectionString = configuration.GetConnectionString("Module4HW5");
+            var connectionString = configuration.GetConnectionString("Module_4.HW_5");
             dbOptionsBuilder.UseSqlServer(connectionString, i => i.CommandTimeout(20));
             var applicationContext = new ApplicationContext(dbOptionsBuilder.Options);
 
@@ -38,7 +39,7 @@ namespace Module_4.HW_5
             applicationContext.Project.Update(project);
             applicationContext.EmployeeProject.UpdateRange(employeeProject);
 
-            applicationContext.Employee.Add(new DataAccess.Entities.Employee { TitleId = 2, EmployeeProject = employeeProject, DateOfBirth = new DateTime(1995, 01, 20), FirstName = "Andrey", LastName = "Kudryashov", HiredDate = new DateTime(2019, 11, 10), OfficeId = 1 });
+            applicationContext.Employee.Add(new Entities.Employee { TitleId = 2, EmployeeProject = employeeProject, DateOfBirth = new DateTime(1995, 01, 20), FirstName = "Andrey", LastName = "Kudryashov", HiredDate = new DateTime(2019, 11, 10), OfficeId = 1 });
             applicationContext.SaveChanges();
 
             var employee = applicationContext.Employee.Where(i => i.LastName == "Kudryashov").ToList();
