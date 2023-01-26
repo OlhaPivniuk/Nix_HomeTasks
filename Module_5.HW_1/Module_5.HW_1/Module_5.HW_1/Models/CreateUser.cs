bc76c34d5df758b50101771eaff7e5f34ee24ec4
OlhaPivniuk <pivnyuk_olga@ukr.net>
2023-01-26 12:49:15 +0200

    Added Module 5 HW 1

---
+++ Module_5.HW_1/Module_5.HW_1/Module_5.HW_1/Models/CreateUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_5.HW_1.Models
+{
+    public record CreateUser(string Name, string Job, int Id, DateTime CreatedAt);
+}
---
+++ Module_5.HW_1/Module_5.HW_1/Module_5.HW_1/Models/PageInfo.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_5.HW_1.Models
+{
+    public class PageInfo<T>
+    {
+        public int Page { get; set; }
+
+        [JsonProperty("per_page")]
+        public int PerPage { get; set; }
+
+        public int Total { get; set; }
+
+        [JsonProperty("total_pages")]
+        public int TotalPages { get; set; }
+
+        public List<T>? Data { get; set; }
+    }
+}
